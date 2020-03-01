70144bcabe5185051594292181d6827ab9ae278c
IronClawTrem <louie.nutman@gmail.com>
2020-03-01 16:51:00 +0000

    make !showff return a value

--- src/game/g_admin.c
+++ src/game/g_admin.c
@@ -8045,15 +8045,19 @@ qboolean G_admin_versions(gentity_t *ent, int skiparg)
 	return qtrue;
 }
 
-static int calc_ff_pct(statsCounters_t *sc) {
-	if (sc->dmgdone + sc->structdmgdone <= 0) {
-		if (sc->ffdmgdone <= 0)
+static int calc_ff_pct( statsCounters_t *sc )
+{
+	if ( sc->dmgdone + sc->structdmgdone <= 0 )
+	{
+		if ( sc->ffdmgdone <= 0 )
 			return 0;
 		else
 			return 100;
-	} // else {
-		// return round((float)sc->ffdmgdone / (sc->ffdmgdone + sc->dmgdone + sc->structdmgdone) * 100);
-	// }
+	}
+	else
+	{
+		return round((float)sc->ffdmgdone / (sc->ffdmgdone + sc->dmgdone + sc->structdmgdone) * 100);
+	}
 }
 
 qboolean G_admin_showff(gentity_t *ent, int skiparg)
